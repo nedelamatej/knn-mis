3afd00bc50b1d34d6b6ae38d7925c4694ca9d85d
Matěj Neděla <nedela.matej@gmail.com>
2026-04-12 15:31:40 +0200

    Add `qwen-prepare-dataset` job

--- src/qwen-prepare-dataset.sh
+++ src/qwen-prepare-dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N arxiv-prepare-dataset
+#PBS -N qwen-prepare-dataset
 #PBS -l select=1:ncpus=1:mem=8gb:scratch_local=128gb
 #PBS -l walltime=24:00:00
 
@@ -8,7 +8,7 @@
 # Vysoke uceni technicke v Brne
 # Fakulta informacnich technologii
 #
-# Nazev: arxiv-prepare-dataset.sh
+# Nazev: qwen-prepare-dataset.sh
 # Autor: David Machu (xmachu05)
 #        Matej Nedela (xnedel11)
 
@@ -16,12 +16,11 @@ set -euo pipefail
 
 cd ${SCRATCHDIR}
 
-[ -f "${PBS_O_WORKDIR}/data.tar" ] \
-  && cp ${PBS_O_WORKDIR}/data.tar . \
-  && tar -xf data.tar
+cp ${PBS_O_WORKDIR}/data.tar .
+tar -xf data.tar
 
-cp ${PBS_O_WORKDIR}/src/arxiv-prepare-dataset.py .
-cp ${PBS_O_WORKDIR}/arxiv-metadata-snapshot.json .
+cp ${PBS_O_WORKDIR}/src/qwen-prepare-dataset.py .
+cp ${PBS_O_WORKDIR}/metadata.json .
 cp ${PBS_O_WORKDIR}/requirements.txt .
 
 module load python
@@ -32,11 +31,10 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python arxiv-prepare-dataset.py -c 50000
+python qwen-prepare-dataset.py
 
-tar -cf data.tar data
-
-cp data.tar ${PBS_O_WORKDIR}
-cp metadata.json ${PBS_O_WORKDIR}
+cp train.json ${PBS_O_WORKDIR}
+cp eval.json ${PBS_O_WORKDIR}
+cp test.json ${PBS_O_WORKDIR}
 
 clean_scratch
